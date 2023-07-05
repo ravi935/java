a218ac1c21451ae1dd24fab1222ead91f1a5818b
Ravi Jiaswal <75163476+ravi935@users.noreply.github.com>
2023-07-06 02:32:44 +0530

    Add files via upload

---
+++ lessorequal.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class lessorequal {
+    public static void main(String arg[]) {
+    Scanner s = new Scanner(System.in);
+    int a = s.nextInt();
+    int b = s.nextInt();
+    if (a == b) {
+        System.out.println("equal");
+    } else if (a > b) {
+            System.out.println("a is greater ");
+        }else {
+            System.out.println("a is lesser ");
+        }
+     }
+}
+
+    
