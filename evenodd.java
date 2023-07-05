42c948cb3ed4fb526961ae4f28e11ba581a29a82
Ravi Jiaswal <75163476+ravi935@users.noreply.github.com>
2023-07-06 01:58:30 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+public class evenodd {
+    public static void main(String arg[]) {
+    Scanner s = new Scanner(System.in);
+    int x = s.nextInt();
+    if (x % 2 == 0) {
+        System.out.println("even");
+    } else{
+        System.out.println("odd");
+    }
+}
+}
+
+
